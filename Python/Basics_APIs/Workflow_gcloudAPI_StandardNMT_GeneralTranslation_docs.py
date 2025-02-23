8a5c4bfc75256536ce5609b1f5dd78010eee0a0c
bmarron18 <soil.foodwebs@gmail.com>
2025-02-22 22:51:31 -0600

    22Feb2025

--- Python/Basics_APIs/Workflow_gcloudAPI_StandardNMT_GeneralTranslation_docs.py
+++ Python/Basics_APIs/Workflow_gcloudAPI_StandardNMT_GeneralTranslation_docs.py
@@ -9,8 +9,7 @@ source: translate_v3beta1_translate_document.py
 
 # %%
 
-# [BEGIN Workflow_gcloudTranslate_v3_fr-compu.docs]
-  # ==> Preliminaries
+=======   Preliminaries   ===========================
 
 # %%
 
@@ -39,45 +38,143 @@ marron_bruce_mx@cloudshell:~ (my-project-uteca1)$
 # %%
 
 '''
-Install gcloud CLI (or SDK) on home compu in /home/bmarron
+Install gcloud SDK on home compu 
+    [==> /home/bmarron/google-cloud-sdk]
 '''
-google-cloud-cli-linux-x86_64.tar.gz
+https://cloud.google.com/sdk/docs/install
+    google-cloud-cli-linux-x86_64.tar.gz
 
   # Run `gcloud cheat-sheet` to see a roster of go-to `gcloud` commands.
 $ cd ~
 $ ./google-cloud-sdk/bin/gcloud cheat-sheet
 
+
+
+
 # %%
 '''
-   Obtain initial Application Default Credentials (ADC)
-   ADC authentication is thru google-cloud-sdk 
-   (browser sent to Google Auth Library; follow browser prompts)
-  [NB. NOT setting up a Service Account; ADC stored locally]
+Check for google-cloud-sdk updates
+Revert google-cloud-sdk to previous version (if needed)
 '''
-  # get ADC credentials (stored locally as .json file)
+    
+$ cd ~
+$ ./google-cloud-sdk/bin/gcloud components update
+
+
+
+To revert your CLI to the previously installed version:
+  $ gcloud components update --version 505.0.0
+ 
+ 
+
+
+# %%
+'''
+Link google-cloud-sdk to Spyder python interpreter
+
+OR
+
+DONT USE !!!
+Link Spyder to google-cloud-sdk Python 
+[bc must install Spyder Kernels] 
+'''
+
+    # The default google-cloud-sdk bundled Python interpreter is here:
+/home/bmarron/google-cloud-sdk/platform/bundledpythonunix/bin/python3.12
+
+    # The default Spyder python interpreter is here:
+/home/bmarron/.local/spyder-6/bin/python3.11
+
+    #re-configure google-cloud-sdk Python interpreter
+$ cd ~
+$ ./google-cloud-sdk/bin/gcloud topic startup
+
+    #set the CLOUDSDK_PYTHON environment variable to Spyder default python interpreter
+$ export CLOUDSDK_PYTHON=/home/bmarron/.local/spyder-6/bin/python3.11
+
+
+DO NOT USE!!
+#To change Python interpreter in Spyder
+# select an option
+#     Internal 
+#     Use the following Python interpreteruse the dropdown below to select your preferred environment.
+#     /home/bmarron/google-cloud-sdk/platform/bundledpythonunix/bin/python3.12
+#NO GO!!
+#An error occurred while starting the kernel
+#The Python environment or installation whose interpreter is located at
+#    /home/bmarron/google‑cloud‑sdk/platform/bundledpythonunix/bin/python3
+#doesn't have spyder‑kernels version >=3.0.0,<3.1.0 installed. Without this module and 
+#specific version is not possible for Spyder to create a console for you. You can install it by 
+#activating your environment (if necessary) and then running in a system terminal:
+#    conda install spyder-kernels=3.0
+#or
+#   pip install spyder-kernels==3.0.*
+
+
+
+
+# %%
+'''
+   Obtain Application Default Credentials (ADC)
+'''
+
+  # Initial get of ADC credentials 
+  # will be stored locally as .json file
+  # ADC authentication is thru google-cloud-sdk 
+  # Browser sent to Google Auth Library; follow browser prompts
+  # NOT setting up a Service Account; ADC stored locally
+ 
 $ cd ~
 $ ./google-cloud-sdk/bin/gcloud auth application-default login
 
-''' Credentials saved to file: 
-[/home/bmarron/.config/gcloud/application_default_credentials.json]
 
-These credentials will be used by any library that requests 
-Application Default Credentials (ADC).'''
+    # once have credentials will be used by any library that requests ADCs
+    # ADC credentials saved here:: 
+/home/bmarron/.config/gcloud/application_default_credentials.json
 
-    # set quota to "my-project-uteca1" in ADC credentials  
+
+
+# %%
+'''
+Check the authentication account used by gcloud
+Set the authentication account (if needed)
+'''
+    #Check current authentication account
+$ cd ~
+$ ./google-cloud-sdk/bin/gcloud auth list
+
+      Credentialed Accounts
+ACTIVE  ACCOUNT
+*       marron.bruce.mx@gmail.com
+
+
+    # set the authentication account (if needed)
+$ gcloud config set account `ACCOUNT`   #<== re-type 'ACCOUNT' w/ apostrophes!!
+
+
+
+# %%
+'''
+Set Google Cloud projects for billing and quotas 
+'''
+
+    # quotas are per project
+    #  set to "my-project-uteca1"
 $ ./google-cloud-sdk/bin/gcloud auth application-default set-quota-project my-project-uteca1
 
-'''Quota project "my-project-uteca1" was added to ADC which can be used by 
-Google client libraries for billing and quota.'''
-
+Quota project "my-project-uteca1" was added to ADC which can be used by 
+Google client libraries for billing and quota.
 
 
 
 # %%
 
-# [BEGIN Workflow_gcloudTranslate_v3_fr-compu.docs]
-  # ==> Normal workflow
+============== Workflow_gcloudAPI_StandardNMT_GeneralTranslation_docs.py  ============
+
+# %%
+
 '''
+Doing complete document translations thru API with the Standard NMT model
   Account and Project Configuration
 '''
 
@@ -86,9 +183,10 @@ Google client libraries for billing and quota.'''
 $ cd ~ 
 $ ./google-cloud-sdk/bin/gcloud init
 
-'''The Google Cloud CLI is configured and ready to use
-Created a default .boto configuration file at [/home/bmarron/.boto]. 
-The Google Cloud CLI is configured and ready to use!'''
+Welcome! This command will take you through the configuration of gcloud.....
+
+The Google Cloud CLI is configured and ready to use
+
 
   
 # %%
@@ -105,7 +203,31 @@ $ virtualenv venv-translate &&
 source venv-translate/bin/activate &&
 pip install ipython google-cloud-translate
 
+# %%
+'''
+Install google-cloud-translate in Spyder Python
+'''
+https://stackoverflow.com/questions/59200541/why-does-my-google-translate-api-work-in-the-terminal-but-not-an-executable
 
+
+https://stackoverflow.com/questions/2915471/install-a-python-package-into-a-different-directory-using-pip
+
+
+
+$ pip install --target /home/bmarron/.local/spyder-6/lib/python3.11/site-packages google-cloud-translate
+/home/bmarron/.local/spyder-6/lib/python3.11/site-packages
+
+
+    # module found? no
+export PYTHONPATH=${PYTHONPATH}:${HOME}/.local/spyder-6/
+
+    # module found? no
+import sys
+sys.path.append('/home/bmarron/.local/spyder-6/lib/python3.11/site-packages')
+    # Now the import will work
+
+    #google-cloud-translate in python 3.10 distro
+google-cloud-translate in ./.local/lib/python3.10/site-packages (3.19.0)
 
 # %%
 '''
@@ -115,11 +237,9 @@ pip install ipython google-cloud-translate
       ==> google.cloud.translate (SDK)
 '''
 
-$ ipython
-
 import os
 from google.cloud import translate
-
+from google.cloud import translate_v3 as translate
 
 
 # %%
