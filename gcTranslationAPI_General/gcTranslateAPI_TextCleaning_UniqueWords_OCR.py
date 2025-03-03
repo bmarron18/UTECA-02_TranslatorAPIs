9376a6807a9981bbc2756254bf5f5ecd3d253bea
bmarron18 <soil.foodwebs@gmail.com>
2025-03-03 13:39:13 -0600

    03Mar2025

--- gcTranslationAPI_General/gcTranslateAPI_TextCleaning_UniqueWords_OCR.py
+++ gcTranslationAPI_General/gcTranslateAPI_TextCleaning_UniqueWords_OCR.py
@@ -16,7 +16,8 @@ print (os.getcwd())
 # %%
 
 '''
-    Step 1. Use sed and 'tr' to
+    TEXT CLEANING
+    Use sed and 'tr' to
 	+ remove all non-printable ASCII characters (garbage characters== !(octal 11-15 || 40-176)).
 	+ remove all punctuation
     + NB. 'tr' uses backslash to denote an octal number.
@@ -30,13 +31,32 @@ $ tr -cd '\11-\15\40-\176' < NAME_of_FILE.txt > clean_test.txt &&
 sed 's/[[:punct:]]//g' < clean_test.txt > TMP_00 &&
 mv TMP_00 clean_test.txt
 
-   
+# %%
+
+from contextlib import chdir
+
+
+with chdir('/home/bmarron/Desktop'):
+
+    # opening and creating new .txt file 
+    with open( 
+            "CivilLaw_Terms.txt", 'r') as r, open( 
+                'output.txt', 'w') as o: 
+      
+                    for line in r: 
+                        #isspace() function 
+                        if not line.isspace(): 
+                            o.write(line) 
+  
+    f = open("output.txt", "r") 
+    print("New text file:\n",f.read())
+
 
   
 
 # %%
 '''
-    Step 2. Get word frequencies
+    Get word frequencies
 '''
 
 import pandas as pd
@@ -59,7 +79,7 @@ with chdir('/home/bmarron/Desktop'):
 # %%
 
 '''
-    USE THIS !!
+    USE THIS for student word lists !!
 '''
     
 import re
@@ -67,7 +87,7 @@ from contextlib import chdir
 
 with chdir('/home/bmarron/Desktop'):
     M = []
-    with open('EXCERPTS_P1_Que-hace-la-SCJN.txt') as f:
+    with open('EXCERPTS_P3_EPA_Guidance-for-SOPs.txt') as f:
         for line in f.readlines():
             for word in line.split():
                 word = re.findall('[A-Za-z]+', word)
