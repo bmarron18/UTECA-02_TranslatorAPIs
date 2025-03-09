507c9201259471caba1db4616e0308fbaee8a54f
bmarron18 <soil.foodwebs@gmail.com>
2025-03-08 22:05:48 -0600

    08Mar2025

--- gcTranslationAPI_General/gcTranslateAPI_TextCleaning_UniqueWords_OCR.py
+++ gcTranslationAPI_General/gcTranslateAPI_TextCleaning_UniqueWords_OCR.py
@@ -17,19 +17,21 @@ print (os.getcwd())
 
 # %%
 '''
+=========  Extraction of Vocab  ==============================
 Step 1
     USE THIS to remove end of line returns from text file
     End of line returns are coded as, /n
     tr is truncate Linux command
+'''
     
-        delete all newlines
+        # delete all newlines
     $ tr --delete '\n' < yourfile.txt
     
-        replace newlines with space
+        # replace newlines with space
     $ tr '\n' ' ' < input_filename
     
-'''
 
+    # USE THIS!!
 $ tr '\n' ' ' < Test.txt > Test2.txt
 
 # %%
@@ -37,8 +39,9 @@ $ tr '\n' ' ' < Test.txt > Test2.txt
 
 '''
 Step 2
-    USE THIS for generating a unique word list from texts
-    Gives single words for student vocab lists
+    *Generating a unique word list from texts
+    *Gives single words for student vocab lists
+'''
 
 https://stackoverflow.com/questions/43025188/regular-expression-re-findall-for-set-of-all-alphabetic-words
 
@@ -49,8 +52,10 @@ reject numbers, underscores, whitespace, quote marks, and other punctuation.
     
 The \p{L} matches all Unicode letters regardless of modifiers passed to the regex compile.
     re.findall(r"[p{L}\-\']+", s)
-'''
-    
+
+ 
+
+    # USE THIS!!   
 import re
 from contextlib import chdir
 
@@ -83,6 +88,7 @@ Step 3
     # https://stackoverflow.com/questions/70730240/q-how-to-find-the-next-word-of-a-specific-word-in-a-txt-file
 
 
+    # USE THIS!!
 from contextlib import chdir
 
 
@@ -98,10 +104,132 @@ with chdir('/home/bmarron/Desktop'):
 
     with open("two-word_terms.txt", "a") as f:
         print(prev_word,"",search_word,"",next_word, file=f)
+
 # %%
+'''    OCR for optical pdf files
+        *create text files for Extraction of Vocab
+'''
+    # STEP 1 ==> pdf to png
+$ pdftoppm -png Webdoc.pdf Webdoc
+
+    # STEP 2 ==> png to txt
+$ tesseract Webdoc-1.png Webdoc-1 --dpi 250^C
+
+    # STEP 3 ==> compile/concatentae text files
+$ cat Webdoc* > complete.txt
+
+# %%
+'''
+    Merge pdf files into one
+'''
+
+
+$ pdfunite in-1.pdf in-2.pdf in-n.pdf ouput.pdf
+
+
+# %%
+
+
+# %%
+'''
+Optical Character Recognition (OCR)
+'''
+
+https://builtin.com/data-science/python-ocr
+
+
+
+    # Install Tesseract in Linux and Python
+    # Successfully installed pytesseract-0.3.13 in /usr/lib/python3/dist-packages
+    # pytesseract in ./.local/lib/python3.10/site-packages (0.3.13)
+
+    # for use in terminal
+$ sudo aptitude install tesseract-ocr-all
+
+    # for use in Python
+$ pip3 install tesseract
+
+
 # %%
 
 '''
+Tesseract   ==> image files only
+            ==> unable to read PDFs. rom a PDF, you can 
+            ===> use another utility first to generate a set of images
+            ==> A single image will represent a single page of the PDF
+            ==> pdftppm utility
+'''
+
+https://www.howtogeek.com/682389/how-to-do-ocr-from-the-linux-command-line-using-tesseract/
+
+-r number
+Specifies the X and Y resolution, in DPI.  The default is 150 DPI.
+
+
+    # pdf to images is turing-01.png" "turing-02.png" etc
+$  pdftoppm -png Web.pdf Web
+
+    # JPEG option
+    <opt>=<val>[,<opt>=<val>]"
+-jpegopt 
+$ pdftoppm -jpegopt=90[,-jpegopt=n] PDF-file PPM-root
+
+
+    # general tesseract
+    # creates "recital.txt" at 150 dpi
+$ tesseract recital-63.png recital --dpi 150
+
+
+    # loop thru turing-xx.png files
+    # create a text file from each image
+$ for i in turing-??.png; do tesseract "$i" "text-$i" -l eng; done;
+
+    # concatenate text files into one file
+$ cat text-turing* > complete.txt
+
+
+
+
+# %%
+
+'''
+build an OCR engine in Python
+'''
+
+import pytesseract
+from PIL import Image
+import pytesseract
+import numpy as np
+
+    # clean image OCR
+filename = "image_01.png" 
+img1 = np.array(Image.open(filename))
+text = pytesseract.image_to_string(img1)
+
+
+
+    # image cleaning then OCR
+import numpy as np
+import cv2norm_img = np.zeros((img.shape[0], img.shape[1]))
+from contextlib import chdir
+
+
+with chdir('/home/bmarron/Desktop'):
+
+    filename = 'image_02.png'
+    img2 = np.array(Image.open(filename))
+    img2 = cv2.normalize(img2, norm_img, 0, 255, cv2.NORM_MINMAX)
+    img2 = cv2.threshold(img2, 100, 255, cv2.THRESH_BINARY)[1]
+    img2 = cv2.GaussianBlur(img2, (1, 1), 0)
+
+    text = pytesseract.image_to_string(img2)
+    print(text)
+
+
+# %%
+
+'''
+=========  Misc Text Options =======================
     Get word frequencies
 '''
 
@@ -248,57 +376,4 @@ list_of_words = data.split()
 next_word = list_of_words[list_of_words.index(search_word) + 1]
 
 
-
-# %%
-
-
-
-'''
-Optical Character Recognition (OCR)
-build an OCR engine in Python
-'''
-
-https://builtin.com/data-science/python-ocr
-
-
-
-    # Install Tesseract. 
-    # Successfully installed pytesseract-0.3.13 in /usr/lib/python3/dist-packages
-    #pytesseract in ./.local/lib/python3.10/site-packages (0.3.13)
-
-    # for use in terminal
-$ sudo aptitude install tesseract-ocr-all
-
-    # foer use in Python
-$ pip3 install tesseract
-
-
-import pytesseract
-from PIL import Image
-import pytesseract
-import numpy as np
-
-    # clean image OCR
-filename = "image_01.png" 
-img1 = np.array(Image.open(filename))
-text = pytesseract.image_to_string(img1)
-
-
-
-    # image cleaning then OCR
-import numpy as np
-import cv2norm_img = np.zeros((img.shape[0], img.shape[1]))
-from contextlib import chdir
-
-
-with chdir('/home/bmarron/Desktop'):
-
-    filename = 'image_02.png'
-    img2 = np.array(Image.open(filename))
-    img2 = cv2.normalize(img2, norm_img, 0, 255, cv2.NORM_MINMAX)
-    img2 = cv2.threshold(img2, 100, 255, cv2.THRESH_BINARY)[1]
-    img2 = cv2.GaussianBlur(img2, (1, 1), 0)
-
-    text = pytesseract.image_to_string(img2)
-    print(text)
 
