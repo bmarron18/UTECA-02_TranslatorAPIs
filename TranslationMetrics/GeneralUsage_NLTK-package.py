eae09a3b582a14400962f5c61820020b2b15e422
bmarron18 <soil.foodwebs@gmail.com>
2025-09-18 12:08:18 -0600

    18Sept2025

--- TranslationMetrics/GeneralUsage_NLTK-package.py
+++ TranslationMetrics/GeneralUsage_NLTK-package.py
@@ -16,6 +16,10 @@ Using the Natural Language Tool Kit (ntlk)
 
 # %%
 
+    # after impot package, need tdownload all the corpus, etc files
+    # opens a separate window to select items
+    # select all
+    
 import nltk
 nltk.download()
 
@@ -28,29 +32,93 @@ Test Code
 
 from nltk.translate.bleu_score import sentence_bleu
 
-# Define your desired weights (example: higher weight for bi-grams)
-weights = (0.25, 0.25, 0, 0)  # Weights for uni-gram, bi-gram, tri-gram, and 4-gram
+    # Define your desired n-gram weights
+    # Weights for uni-gram, bi-gram, tri-gram, and 4-gram
+weights = (0.25, 0.25, 0, 0) 
 
-# Reference and predicted texts (same as before)
+    # Reference and predicted texts (same as before)
+    # These are tokenized lists (not tokenized strings)
 reference = [["the", "picture", "is", "clicked", "by", "me"],
              ["this", "picture", "was", "clicked", "by", "me"]]
 predictions = ["the", "picture", "the", "picture", "by", "me"]
 
-# Calculate BLEU score with weights
+    # Calculate BLEU score with weights
 score = sentence_bleu(reference, predictions, weights=weights)
 print(score)
 
 # %%
 
-#from nltk.translate.bleu_score import corpus_bleu
-
 from nltk.tokenize import sent_tokenize
 
-text = "NLTK is a great NLP toolkit. It makes processing text easy!"
+    # tokenizes individual strings to a SINGLE list
+
+text = "NLTK is a great NLP toolkit. It makes processing text easy!\
+    This is a single string with three sentences"
 sentences = sent_tokenize(text)
 print(sentences)
 
+
 # %%
+from nltk.tokenize import sent_tokenize
+
+    # This is a list of three(3) strings with sentences in each string
+text_list = [
+        "This is the first sentence. This is the second sentence in the first string.",
+        "Another string of text. It has multiple sentences too.",
+        "A single sentence string."
+    ]
+    
+    #create empty list
+tokenized_sentences_list = []
+
+
+for text in text_list:
+        sentences = sent_tokenize(text)
+        tokenized_sentences_list.append(sentences)
+        
+print(tokenized_sentences_list)
+
+# %%
+
+from nltk.tokenize import word_tokenize
+
+sentences = [
+        "This is the first sentence.",
+        "And this is the second one.",
+        "NLTK makes tokenization easy."
+    ]
+
+tokenized_sentences = [word_tokenize(sentence) for sentence in sentences]
+
+print(tokenized_sentences)
+
+
+# %%
+
+'''
+Tokenizing
+https://www.nltk.org/howto/tokenize.html
+
+'''
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+    # text is a single string with multiple sentences
+text = "NLTK is a powerful library for natural language processing. \
+    It's widely used! You can do a lot with it. And a third sentence."
+
+sentences = sent_tokenize(text, "english")
+words = word_tokenize(text, "english")
+
+print(sentences)
+
+#print(words)
+
+# %%
+
+'''
+Check tokenized output
+
+'''
 
 import os
 from nltk.tokenize import sent_tokenize, word_tokenize
@@ -67,18 +135,21 @@ output_filepath = os.path.join(doc_dir, doc_to_print)
 reference_text_1 = "This is a long and complex reference text for evaluating the machine translation output. It contains multiple sentences and aims to provide a comprehensive example."
 reference_text_2 = "Another reference text that is also long and provides a different perspective on the same topic. This helps in capturing more nuances."
 
+
+    # original code
 references = [
         [word_tokenize(sent) for sent in reference_text_1.split('. ') if sent],
         [word_tokenize(sent) for sent in reference_text_2.split('. ') if sent]
-    ]
+        ]
 
     # If the list contains elements of other data types 
     # you need to convert them to strings before joining
     # use map(str, <list_name>)
-refs_string = ",".join(map(str, references))
+#refs_string = ",".join(map(str, references))
 
 with open(output_filepath, "w", encoding="utf-8") as f:
-     f.write(refs_string)
+     f.write(references)
+#     f.write(refs_string)
 
 #print(references)
 
@@ -155,23 +226,6 @@ score = single_meteor_score(ref, hypo)
 # Print the result 
 print(f"METEOR Score: {score:.3f}")
 
-# %%
-'''
-Tokenizing
-
-'''
-
-import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-text = "NLTK is a powerful library for natural language processing. It's widely used! You can do a lot with it."
-sentences = sent_tokenize(text)
-sent_strg = "".join(sentences)
-words = word_tokenize(sent_strg)
-
-print(sentences)
-
-print(words)
 
 # %%
 
