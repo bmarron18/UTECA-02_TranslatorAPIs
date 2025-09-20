f83aeddd84f7af68c18664f5c7a738aa2b2d4a8a
bmarron18 <soil.foodwebs@gmail.com>
2025-09-19 22:03:49 -0600

    19Sept2025

--- Translation_APIs/TranslationAPI_OpenAI/OpenAI_API.py
+++ Translation_APIs/TranslationAPI_OpenAI/OpenAI_API.py
@@ -7,46 +7,157 @@ Spyder Editor
 """
 
 '''
-https://community.openai.com/t/which-api-for-translation/553000/6
-GPT3.5
-I translated recently a large document, close to 4000 pages (in chunks of about 8 pages per API call, it ran for 11 hours), from English into Spanish, using gpt3.5, and I was very satisfied with the result.
-
-According to our findings, about 75% of the translations were accurate. However, in the context of running an online business, the difference between 75% and 100% can be the difference between making a sale and losing a customer’s interest, as they can immediately tell if the translation is not professional. AI-generated translations should always be paired with human quality assurance. In fact, we have introduced a separate service that offers just that—human reviewers for AI-translated content, for companies that want to leverage AI but need their content to be flawless.
-
-'''
-
-
-'''
-https://community.openai.com/t/does-the-api-for-translations-actually-translate/733078/2
-I’ve got a simple spoken audio to text interface working, using Python and the async version of OpenAI official python library.
-
-It works great for spoken English, but does not translate any non-English to English.
-
-The OpenAI API documentation says it translates non-English to English and lists a large series of languages. I see people posting apps where they say they are calling the OpenAI API for their translations.
-
-I’ve seen posts where people describe doing this using curl, and using a v1 url to whisper-1, but I cannot describe this anymore without this moderation system claiming I’m trying to hide a link in my post.
-
-How is anyone achieving translation? I’ve tried using the optional prompt, tried the ‘language=“en”’ optional parameter. It always transcribes into the original language and does not translate to English. How to trigger the translation? Does anyone have this working through the OpenAI API and not Whisper running locally?
-
-transcript = openai.audio.transcriptions.create(model=“whisper-1”, file=audio_file)
-
-translation = openai.audio.translations.create( model =“whisper-1”,
-file= audio_file,
-temperature = 0.90)
-
-For english audio to english text, I use transcriptions.
-
-For non-english to english, I use translation. The list of languages available is listed in the documentation for whisper. The system auto-detects the language and it is not necessary to set a variable.
-'''
-
-
-'''
-https://transcy.crisp.help/en/article/how-to-use-openai-translation-vni6lx/
-
 https://platform.openai.com/docs/quickstart
 
+https://community.openai.com/t/which-api-for-translation/553000/6
+https://platform.openai.com/docs/api-reference/responses
+
+https://platform.openai.com/docs/api-reference/responses/create
+https://platform.openai.com/docs/guides/migrate-to-responses
+https://platform.openai.com/docs/guides/pdf-files?api-mode=responses
+
+https://community.openai.com/t/translation-script-not-working/896719/2
+https://platform.openai.com/docs/api-reference/chat/create?lang=python
+
 '''
+# %%
+
+    # Response API usage	<== USE THIS for .pdf / .txt translation!!
+    # Install OpenAI SDK into conda env, (spyder_env)
+    
+$ conda activate spyder_env
+(spyder_env)$ conda install openai
+(spyder_env)$ conda deactivate
 
 
+
+# %%
+
 '''
-USE THIS!!
+Simple Test Run
+
+'''
+
+from openai import OpenAI
+
+#client = OpenAI(api_key=API_KEY)
+client = OpenAI(api_key="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")
+
+response = client.responses.create(
+  model = "gpt-4o",
+  temperature = 1.3,
+  instructions = "Talk like a pirate.",
+  input = "Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
+
+'''
+Arr matey, once upon a time, atop the moonlit waves o' dreamland, pranced a 
+shimmering unicorn, whose horn sparkled with such magic it sent the young 
+pirate driftin' to sleep among the stars.
+'''
+
+# %%
+
+	# Upload a .pdf / .txt referenced in input = []
+file = client.files.create(
+    file=open("draconomicon.pdf", "rb"),
+    purpose="user_data",
+)
+
+
+# %%
+
+
+from openai import OpenAI
+from pathlib import Path
+import os
+
+
+#client = OpenAI(api_key=API_KEY)
+client = OpenAI(api_key="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")
+
+
+
+# --- Define Files and File Paths ---
+    # Create one (1) empty file on the Desktop
+        # ==> OUTPUT_FILE will hold the Translated text from the AI
+        # ==> file extension must be .pdf
+    # Select .pdf file to be translated and place in Desktop
+        # ==> INPUT_FILE will hold the .pdf To Be Translated
+
+    # watch file name for INPUT_FILE
+        # ==> strictly alpha! 
+        # ==> NO dashes or underline (won't upload to Gemini)
+    
+    # Change the language names in the files as needed
+    # Change the file extension of OUTPUT_FILE as needed
+    
+INPUT_FILE = "englishTBT.pdf"  
+OUTPUT_FILE = "spanish_T.txt"
+    
+    
+    # set up the file paths for the input / output files
+    # set the file path to your Desktop
+    
+doc_to_translate = INPUT_FILE
+doc_to_print = OUTPUT_FILE
+doc_dir = "/home/bmarron/Desktop"
+input_filepath = os.path.join(doc_dir, doc_to_translate)
+output_filepath = os.path.join(doc_dir, doc_to_print)
+
+    # Retrieve the UNPUT_FILE as PosixPath
+filepath = Path(input_filepath)
+
+
+
+	# Upload a .txt referenced in input = []
+file = client.files.create(
+    file=open(filepath, "rb"),
+    purpose="user_data",
+)
+
+	# Developer level
+sys_prompt = "You are a linguistics expert specializing in translations. \
+    Do not provide additional commentary. Just perform the task at hand."
+
+	# User level
+user_prompt= "Translate the uploaded file to standard Spanish."
+
+
+
+	# API call
+response = client.responses.create(
+    model="gpt-4o",
+    temperature=1.3,
+    instructions= sys_prompt,
+    input=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "input_file",
+                    "file_id": file.id,
+                },
+                {
+                    "type": "input_text",
+                    "text": user_prompt,
+                },
+            ]
+        }
+    ]
+)
+
+	# API output to IPython
+#print(response.output_text)
+
+
+#--- Output to OUTPUT_FILE---
+
+with open(output_filepath, "w", encoding="utf-8") as f:
+     f.write(response.output_text)
+     
+print(f"Translation complete! Translated text saved to '{output_filepath}'.")
+
+
